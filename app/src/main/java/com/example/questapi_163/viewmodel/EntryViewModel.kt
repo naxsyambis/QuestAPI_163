a9abd04a919b523ad1220569d99a0a46012881ac
naxsyambis <naxsyambis@gmail.com>
2025-12-19 23:14:52 +0700

    implementasi validasi input dan fungsi tambah siswa pada EntryViewModel

--- app/src/main/java/com/example/questapi_163/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/EntryViewModel.kt
@@ -1,10 +1,57 @@
 package com.example.questapi_163.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.questapi_163.modeldata.DetailSiswa
+import com.example.questapi_163.modeldata.UIStateSiswa
+import com.example.questapi_163.modeldata.toDataSiswa
 import com.example.questapi_163.repositori.RepositoryDataSiswa
 
 class EntryViewModel(
     private val repositoryDataSiswa: RepositoryDataSiswa
 ) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return uiState.nama.isNotBlank()
+                && uiState.alamat.isNotBlank()
+                && uiState.telpon.isNotBlank()
+                && uiState.telpon.all { it.isDigit() } // 🔴 PENTING
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    suspend fun addSiswa() {
+        if (!validasiInput()) {
+            println("Validasi gagal")
+            return
+        }
+
+        try {
+            val response = repositoryDataSiswa.postDataSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+
+            if (response.isSuccessful) {
+                println("✅ Sukses tambah data")
+            } else {
+                println("❌ Gagal: ${response.errorBody()?.string()}")
+            }
+
+        } catch (e: Exception) {
+            println("🔥 Error kirim data: ${e.message}")
+        }
+    }
 
 }
