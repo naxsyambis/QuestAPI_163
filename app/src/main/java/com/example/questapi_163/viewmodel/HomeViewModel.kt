f8a92720d55f0c6cd3e9421b92449e2ac8440327
naxsyambis <naxsyambis@gmail.com>
2025-12-19 22:52:10 +0700

    buat sealed interface StatusUiSiswa untuk state management

---
+++ app/src/main/java/com/example/questapi_163/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_163.viewmodel
+
+import com.example.questapi_163.modeldata.DataSiswa
+
+sealed interface StatusUiSiswa {
+    data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
