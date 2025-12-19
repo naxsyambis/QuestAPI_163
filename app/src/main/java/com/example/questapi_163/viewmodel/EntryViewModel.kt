b9961c5b7951b7ecaddd54a5d553cba760a97aba
naxsyambis <naxsyambis@gmail.com>
2025-12-19 23:12:41 +0700

    buat EntryViewModel untuk menangani input data siswa

---
+++ app/src/main/java/com/example/questapi_163/viewmodel/EntryViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.questapi_163.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.questapi_163.repositori.RepositoryDataSiswa
+
+class EntryViewModel(
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+}
