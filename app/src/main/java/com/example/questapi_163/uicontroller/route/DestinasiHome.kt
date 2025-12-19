71093d00b8210ab58c547dd47b40f0251cc6e4ae
naxsyambis <naxsyambis@gmail.com>
2025-12-19 23:39:11 +0700

    Destinasi Home pada destinasi navigasi

---
+++ app/src/main/java/com/example/questapi_163/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.questapi_163.uicontroller.route
+
+import com.example.questapi_163.R
+
+object DestinasiHome : DestinasiNavigasi {
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
