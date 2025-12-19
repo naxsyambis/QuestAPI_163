a652980aa1c851a3e5e30dc9ddef4f6577fc1906
naxsyambis <naxsyambis@gmail.com>
2025-12-19 23:15:21 +0700

    Memperbaiki home view model

--- app/src/main/java/com/example/questapi_163/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/HomeViewModel.kt
@@ -17,6 +17,8 @@ sealed interface StatusUiSiswa {
     object Loading : StatusUiSiswa
 }
 
+
+
 class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
     var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
