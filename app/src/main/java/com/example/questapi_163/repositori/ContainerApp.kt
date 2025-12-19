6bb8aaaa54d4c3bbdd11722d960f4313ab3ed8cb
naxsyambis <naxsyambis@gmail.com>
2025-12-19 22:40:41 +0700

    konfigurasi Retrofit dan integrasi JaringanRepository

--- app/src/main/java/com/example/questapi_163/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_163/repositori/ContainerApp.kt
@@ -14,3 +14,36 @@ interface ContainerApp{
     val repositoryDataSiswa: RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp{
+
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level= HttpLoggingInterceptor.Level.BODY
+
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService) }
+}
+
