529637b0b092eaf0821cb40c82692e314e604be5
naxsyambis <naxsyambis@gmail.com>
2025-12-19 22:25:37 +0700

    buat interface service API untuk data siswa

---
+++ app/src/main/java/com/example/questapi_163/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.questapi_163.repositori
+
+import com.example.questapi_163.apiservice.ServiceApiSiswa
+import com.example.questapi_163.modeldata.DataSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
