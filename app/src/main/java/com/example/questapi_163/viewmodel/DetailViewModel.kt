fa16db9b178dda53c8f7f607bdc48bba679a9ded
naxsyambis <naxsyambis@gmail.com>
2025-12-26 22:19:06 +0700

    tambah StatusUIDetail untuk manajemen state halaman detail

--- app/src/main/java/com/example/questapi_163/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/DetailViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_163.viewmodel
 
+import com.example.questapi_163.modeldata.DataSiswa
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
