376a9a8e5cb66166cc3b38261ec78a20916eb504
naxsyambis <naxsyambis@gmail.com>
2025-12-26 22:44:07 +0700

    implementasi fungsi editSatuSiswa untuk menyimpan perubahan

--- app/src/main/java/com/example/questapi_163/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/EditViewModel.kt
@@ -39,4 +39,15 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
         }
     }
 
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
 }
