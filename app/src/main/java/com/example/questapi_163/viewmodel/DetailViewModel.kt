7c6eb2a358d5986065d4e6c9f7c43acabd9d365a
naxsyambis <naxsyambis@gmail.com>
2025-12-26 22:53:19 +0700

    implementasi pengambilan data siswa pada DetailViewModel

--- app/src/main/java/com/example/questapi_163/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/DetailViewModel.kt
@@ -1,10 +1,48 @@
 package com.example.questapi_163.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_163.modeldata.DataSiswa
+import com.example.questapi_163.repositori.RepositoryDataSiswa
+import com.example.questapi_163.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = com.example.questapi_163.viewmodel.StatusUIDetail.Loading
+            statusUIDetail = try {
+                com.example.questapi_163.viewmodel.StatusUIDetail.Success(
+                    satusiswa = repositoryDataSiswa.getSatuSiswa(
+                        idSiswa
+                    )
+                )
+            } catch (e: IOException) {
+                com.example.questapi_163.viewmodel.StatusUIDetail.Error
+            } catch (e: HttpException) {
+                com.example.questapi_163.viewmodel.StatusUIDetail.Error
+            }
+        }
+    }
 }
