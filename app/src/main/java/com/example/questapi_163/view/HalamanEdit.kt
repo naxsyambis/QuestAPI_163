b0611963d3071fa621c4d670741a95412286a277
naxsyambis <naxsyambis@gmail.com>
2025-12-26 22:47:00 +0700

    tambah komponen EditSiswaScreen untuk pembaruan data

--- app/src/main/java/com/example/questapi_163/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_163/view/HalamanEdit.kt
@@ -1,4 +1,17 @@
 package com.example.questapi_163.view
 
-class HalamanEdit {
-}
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_163.viewmodel.EditViewModel
+import com.example.questapi_163.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
