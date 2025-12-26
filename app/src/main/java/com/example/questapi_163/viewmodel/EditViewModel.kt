9412b63c91016139b5bfe3213da0e5f3a0648d0f
naxsyambis <naxsyambis@gmail.com>
2025-12-26 22:40:11 +0700

    tambah EditViewModel untuk mengelola state pembaruan data

--- app/src/main/java/com/example/questapi_163/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_163/viewmodel/EditViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.questapi_163.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_163.modeldata.DetailSiswa
+import com.example.questapi_163.modeldata.UIStateSiswa
+import com.example.questapi_163.modeldata.toDataSiswa
+import com.example.questapi_163.modeldata.toUiStateSiswa
+import com.example.questapi_163.repositori.RepositoryDataSiswa
+import com.example.questapi_163.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+}
